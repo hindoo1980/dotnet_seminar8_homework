33bf8671b82e5df6fa2d1f4e689099f918dedfa3
DENIS <hindoo@mail.ru>
2022-11-28 01:05:35 +0300

    first

---
+++ exercises/seminar8_54/Program.cs
@@ -0,0 +1,105 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] arrCreate ()
+
+    {
+        int m;
+        int n;
+
+        Random rndInt = new Random();
+
+        m = rndInt.Next(2, 9);
+        n = rndInt.Next(2, 9);
+
+        int[,] nums = new int[m,n];
+
+        for (int i = 0; i < nums.GetLength(0) ; i++ )
+        {
+            for (int j = 0; j < nums.GetLength(1) ; j++ )
+            {
+            nums[i,j] = rndInt.Next(0, 9);
+            }
+        }
+    
+        return nums;
+    }
+
+void ColumnSort ()
+
+    {
+
+        int[,] nums = arrCreate ();
+
+        Console.WriteLine();
+
+        Console.WriteLine("для следующего массива :");
+
+        Console.WriteLine();
+
+
+        for (int i = 0; i < nums.GetLength(0) ; i++ )
+
+        {
+            for (int j = 0; j < nums.GetLength(1) ; j++ )
+            {
+                Console.Write(nums[i,j]+" ");
+            }
+
+        Console.WriteLine();
+
+        }
+
+
+        int temp;
+
+        int rowLenght = nums.GetLength(0);
+        int colLenght = nums.GetLength(1);
+
+        
+        for (int i = 0; i < rowLenght ; i++ )
+        {
+
+            for (int j = 0; j < colLenght ; j++ )
+            {
+                    for (int k = j + 1; k < colLenght ; k++ )
+                    {
+
+                        if (nums[i,j] < nums[i,k])
+                        {
+                            temp = nums[i,j];
+
+                            nums[i,j] = nums[i,k];
+
+                            nums[i,k] = temp;
+                        }
+
+
+                    }
+              
+            }
+
+        }
+
+
+        Console.WriteLine();
+
+        Console.WriteLine("получаем вариант с отсортированными по убыванию строками:");
+
+        Console.WriteLine();
+
+
+        for (int i = 0; i < nums.GetLength(0) ; i++ )
+
+        {
+            for (int j = 0; j < nums.GetLength(1) ; j++ )
+            {
+                Console.Write(nums[i,j]+" ");
+            }
+
+        Console.WriteLine();
+
+        }
+    
+    }
+
+ ColumnSort ();
